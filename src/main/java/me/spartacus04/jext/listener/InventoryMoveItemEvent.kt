98a06b2de1815bd21f7a2022a708cfb7d91ac3e6
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2023-03-23 13:00:27 +0100

    Update InventoryMoveItemEvent.kt

--- src/main/java/me/spartacus04/jext/listener/InventoryMoveItemEvent.kt
+++ src/main/java/me/spartacus04/jext/listener/InventoryMoveItemEvent.kt
@@ -6,18 +6,11 @@ import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryMoveItemEvent
-import org.bukkit.event.inventory.InventoryType
 
 internal class InventoryMoveItemEvent : Listener {
     @EventHandler
     fun inventoryMoveItemEvent(e: InventoryMoveItemEvent) {
-        return
-
-        if(e.source.location != null) {
-            if(e.source.location!!.block.type == Material.JUKEBOX) {
-                DiscPlayer(DiscContainer(e.item)).stop(e.source.location!!)
-            }
-        } else if(e.destination.location != null) {
+        if(e.destination.location != null) {
             if(e.destination.location!!.block.type == Material.JUKEBOX) {
                 DiscPlayer(DiscContainer(e.item)).play(e.destination.location!!)
             }
