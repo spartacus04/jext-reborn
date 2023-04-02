3f1c98e5da9eb1075bad9d77941ef0c8489d0df3
Andrew <52862005+spartacus04@users.noreply.github.com>
2023-04-02 18:44:54 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ import type { SongData } from '@/config';
 import { pack_icon } from './assets';
 
 
-export const versionStore = writable(12);
+export const versionStore = writable(13);
 export const nameStore = writable('your_pack_name');
 export const iconStore = writable(pack_icon);
 
