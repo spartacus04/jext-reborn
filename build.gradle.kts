b6a94620bf2afa9d33bedcdddb2b625be8ed508e
Andrew <52862005+spartacus04@users.noreply.github.com>
2023-04-12 16:08:47 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     kotlin("jvm") version "1.8.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
     `maven-publish`
 }
 
