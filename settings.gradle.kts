e80551a913455b78c1b1764fc1b698832631432d
codefactor-io <support@codefactor.io>
2023-04-12 14:08:02 +0000

    [CodeFactor] Apply fixes to commit d73bbe1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@
 rootProject.name = "JEXT-Reborn"
 
 toolchainManagement {
-    jvm { 
+    jvm {
         javaRepositories {
-            repository("foojay") { 
+            repository("foojay") {
                 resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
             }
         }
