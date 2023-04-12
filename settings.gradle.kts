2f7babc88e50ba04bb86b7e57f3ee898f1590154
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2023-04-12 16:12:42 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,3 @@ rootProject.name = "JEXT-Reborn"
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
 }
-
-toolchainManagement {
-    jvm { 
-        javaRepositories {
-            repository("foojay") { 
-                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
-            }
-        }
-    }
-}
