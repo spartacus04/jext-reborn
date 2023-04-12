d73bbe1595bc55014f6aaba4f04390f13192dbca
Andrew <52862005+spartacus04@users.noreply.github.com>
2023-04-12 16:07:52 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 
 rootProject.name = "JEXT-Reborn"
+
+toolchainManagement {
+    jvm { 
+        javaRepositories {
+            repository("foojay") { 
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
