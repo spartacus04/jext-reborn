9c9caf46abe47fad1b3bc074b626f7e040c614fa
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2023-07-17 16:48:14 +0200

    Update JukeboxEventListener.kt

--- src/main/java/me/spartacus04/jext/listener/JukeboxEventListener.kt
+++ src/main/java/me/spartacus04/jext/listener/JukeboxEventListener.kt
@@ -10,6 +10,7 @@ import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.block.Jukebox
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockBreakEvent
