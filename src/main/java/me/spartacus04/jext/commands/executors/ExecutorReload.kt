43cf69e08f1140e8e27eac0df62a8d5e8a5562e7
spartacus04 <andrea.bonari.pingas@gmail.com>
2024-04-08 12:21:58 +0200

    Update PUBLIC_IP on reload

--- src/main/java/me/spartacus04/jext/commands/executors/ExecutorReload.kt
+++ src/main/java/me/spartacus04/jext/commands/executors/ExecutorReload.kt
@@ -1,11 +1,14 @@
 package me.spartacus04.jext.commands.executors
 
+import me.spartacus04.jext.State
 import me.spartacus04.jext.State.CONFIG
 import me.spartacus04.jext.State.DISCS
 import me.spartacus04.jext.State.INTEGRATIONS
+import me.spartacus04.jext.State.PUBLIC_IP
 import me.spartacus04.jext.State.WEBSERVER
 import me.spartacus04.jext.commands.adapter.ExecutorAdapter
 import me.spartacus04.jext.utils.JextMetrics
+import me.spartacus04.jext.utils.PublicIP
 import me.spartacus04.jext.utils.sendJEXTMessage
 import org.bukkit.command.CommandSender
 
@@ -26,6 +29,10 @@ internal class ExecutorReload : ExecutorAdapter("jextreload", "reload") {
         INTEGRATIONS.reloadDefaultIntegrations()
         JextMetrics.reloadMetrics()
         WEBSERVER.reload()
+
+        PublicIP().getIP {
+            PUBLIC_IP = it
+        }
 
         sender.sendJEXTMessage("reloaded")
     }
