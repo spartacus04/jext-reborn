fc696c88deebde320cc84f4104d4f379930daa3f
spartacus04 <andrea.bonari.pingas@gmail.com>
2024-04-16 13:39:10 +0200

    Update ResourceStatusEvent.kt

--- src/main/java/me/spartacus04/jext/listeners/ResourceStatusEvent.kt
+++ src/main/java/me/spartacus04/jext/listeners/ResourceStatusEvent.kt
@@ -2,32 +2,35 @@ package me.spartacus04.jext.listeners
 
 import me.spartacus04.jext.State.CONFIG
 import me.spartacus04.jext.State.LANG
-import me.spartacus04.jext.State.PLUGIN
+import me.spartacus04.jext.State.SCHEDULER
 import me.spartacus04.jext.listeners.utils.JextListener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerResourcePackStatusEvent
-import org.bukkit.scheduler.BukkitRunnable
 
 internal class ResourceStatusEvent : JextListener() {
     @EventHandler
     fun onResourceStatus(e: PlayerResourcePackStatusEvent) {
         val status = e.status
 
-        object : BukkitRunnable() {
-            override fun run() {
-                if (status == PlayerResourcePackStatusEvent.Status.DECLINED && CONFIG.FORCE_RESOURCE_PACK) {
-                    return e.player.kickPlayer(LANG.getKey(
+        SCHEDULER.runTaskLater({
+            if(!CONFIG.FORCE_RESOURCE_PACK) return@runTaskLater
+
+            when(status) {
+                PlayerResourcePackStatusEvent.Status.DECLINED -> {
+                    return@runTaskLater e.player.kickPlayer(LANG.getKey(
                         e.player,
                         "resource-pack-decline-kick-message"
                     ))
                 }
-                if (status == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD && CONFIG.FORCE_RESOURCE_PACK) {
-                    return e.player.kickPlayer(LANG.getKey(
+                PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD -> {
+                    return@runTaskLater e.player.kickPlayer(LANG.getKey(
                         e.player,
                         "failed-download-kick-message"
                     ))
                 }
+
+                else -> Unit
             }
-        }.runTaskLater(PLUGIN, 100)
+        }, 100)
     }
 }
