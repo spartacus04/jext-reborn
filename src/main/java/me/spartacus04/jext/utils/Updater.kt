8e2b54b8c2c95d9f84a5fed53acd7b7e0c44c2df
spartacus04 <andrea.bonari.pingas@gmail.com>
2024-04-29 08:44:49 +0200

    Update Updater.kt

--- src/main/java/me/spartacus04/jext/utils/Updater.kt
+++ src/main/java/me/spartacus04/jext/utils/Updater.kt
@@ -8,19 +8,19 @@ import java.io.InputStreamReader
 import java.net.URI
 
 internal class Updater {
-        fun getVersion(consumer: (String) -> Unit) {
-            SCHEDULER.runTaskAsynchronously {
-                try {
-                    val reader = BufferedReader(InputStreamReader(URI("https://api.github.com/repos/spartacus04/jext-reborn/releases/latest").toURL().openStream()))
-                    val text = reader.use {
-                        it.readText()
-                    }
-
-                    val version = Regex("\"tag_name\": ?\"([^\"]+)\"").find(text)?.groupValues?.get(1)!!
-                    consumer(version)
-                } catch (exception: IOException) {
-                    PLUGIN.logger.info("Unable to check for updates: " + exception.message)
+    fun getVersion(consumer: (String) -> Unit) {
+        SCHEDULER.runTaskAsynchronously {
+            try {
+                val reader = BufferedReader(InputStreamReader(URI("https://api.github.com/repos/spartacus04/jext-reborn/releases/latest").toURL().openStream()))
+                val text = reader.use {
+                    it.readText()
                 }
+
+                val version = Regex("\"tag_name\": ?\"([^\"]+)\"").find(text)?.groupValues?.get(1)!!
+                consumer(version)
+            } catch (exception: IOException) {
+                PLUGIN.logger.info("Unable to check for updates: " + exception.message)
             }
         }
+    }
 }
