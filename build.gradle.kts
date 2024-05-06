d45c743c61db6e77a3254e97881cc160dfd892bf
spartacus04 <andrea.bonari.pingas@gmail.com>
2024-05-06 08:51:32 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.3")
     compileOnly("org.scala-lang:scala-library:2.13.14")
     compileOnly("com.github.techFortress:GriefPrevention:17.0.0")
-    compileOnly("org.geysermc.geyser:api:2.3.0-SNAPSHOT")
+    compileOnly("org.geysermc.geyser:api:2.2.0-SNAPSHOT")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
     implementation("io.github.bananapuncher714:nbteditor:7.19.2")
