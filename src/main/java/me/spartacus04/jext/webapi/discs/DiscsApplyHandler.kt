9bf8e64e2949d022e77c26acf0767ea54a40a66c
spartacus04 <andrea.bonari.pingas@gmail.com>
2025-03-04 17:43:22 +0100

    Update DiscsApplyHandler.kt

--- src/main/java/me/spartacus04/jext/webapi/discs/DiscsApplyHandler.kt
+++ src/main/java/me/spartacus04/jext/webapi/discs/DiscsApplyHandler.kt
@@ -7,7 +7,6 @@ import me.spartacus04.jext.JextState.DISCS
 import me.spartacus04.jext.JextState.INTEGRATIONS
 import me.spartacus04.jext.JextState.PLUGIN
 import me.spartacus04.jext.gui.JukeboxGui
-import me.spartacus04.jext.utils.sendResourcePack
 import me.spartacus04.jext.webapi.utils.JextHttpHandler
 
 internal class DiscsApplyHandler : JextHttpHandler(true) {
